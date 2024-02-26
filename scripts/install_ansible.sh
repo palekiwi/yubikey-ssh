b0ceeb291a52353fd940a81ca28cecf17889906c
Pale Kiwi <contact@palekiwi.com>
2024-02-26 12:38:44 +0800

    [dev]: add install_ansible.sh

---
+++ scripts/install_ansible.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dir=/tmp/venv/ansible
+
+if [ ! -d $dir ]; then
+    echo "Creating venv dir..."
+    mkdir -p $dir
+fi
+
+echo "Creating python venv in ${dir}..."
+python -m venv $dir
+
+echo "Activating venv..."
+source $dir/bin/activate
+
+echo "Installing ansible..."
+pip install ansible
+
+echo -e "\nDONE"
